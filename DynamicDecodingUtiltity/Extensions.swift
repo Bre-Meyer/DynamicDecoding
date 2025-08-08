b15739cfa6e6c560562580e0a96f640ef7012771
Bre Meyer <bmeyer@theknotww.com>
2025-08-08 15:45:04 -0500

    make extensions public

--- DynamicDecodingUtiltity/Extensions.swift
+++ DynamicDecodingUtiltity/Extensions.swift
@@ -1,19 +1,19 @@
 import Foundation
 
 // MARK: - Decoding Extensions
-extension JSONDecoder {
+public extension JSONDecoder {
     func decode<T: Decodable>(_ type: T.Type, from data: Data, path: DynamicDecodingPath) throws -> T {
         try decode(DynamicDecodingContainer.self, from: data).decode(type, path: path)
     }
 }
 
-extension UnkeyedDecodingContainer {
+public extension UnkeyedDecodingContainer {
     mutating func decode<T: Decodable>(_ type: T.Type, path: DynamicDecodingPath) throws -> T {
         try DynamicDecodingContainer(from: self).decode(type, path: path)
     }
 }
 
-extension KeyedDecodingContainer {
+public extension KeyedDecodingContainer {
     func decode<T: Decodable>(_ type: T.Type, path: DynamicDecodingPath) throws -> T {
         try DynamicDecodingContainer(from: self).decode(type, path: path)
     }
