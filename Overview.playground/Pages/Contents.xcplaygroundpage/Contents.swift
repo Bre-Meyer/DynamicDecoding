b9ecb0d2978c099aec099d9ec9fd89cfa3f44c52
Bre Meyer <bmeyer@theknotww.com>
2025-08-08 17:21:56 -0500

    remove unamed content page

--- Overview.playground/Pages/Contents.xcplaygroundpage/Contents.swift
+++ Overview.playground/Pages/Contents.xcplaygroundpage/Contents.swift
@@ -137,3 +137,16 @@ do {
  - Audience: iOS team
  - Purpose: Make deep JSON reads ergonomic and type‑safe.
  */
+
+private enum Example1 {
+    static let fileName: String = "sampleJSON1"
+}
+
+do {
+    let data = try getData(from: .jsonResource(Example1.fileName))
+    let jsonDecoder = JSONDecoder()
+
+    let model = try jsonDecoder.decode(Designer.self, from: data, path: \.xo_metadata.entities.0.terms.designer)
+} catch {
+    print(error)
+}
